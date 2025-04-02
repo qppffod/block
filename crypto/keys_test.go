578b62df7d7acd0105f5102fb5fc9d35640658f0
Artur <art67789@gmail.com>
2025-04-02 23:24:12 +0300

    Add test publik key len correctness, test for private key sign with correct and incorrect secret message

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -8,6 +8,19 @@ import (
 
 func TestGeneratePrivateKey(t *testing.T) {
 	privKey := GeneratePrivateKey()
-
 	assert.Equal(t, privateKeyLen, len(privKey.Bytes()))
+
+	pubKey := privKey.Public()
+	assert.Equal(t, publicKeyLen, len(pubKey.Bytes()))
+}
+
+func TestPrivateKeySign(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	msg := []byte("some message")
+
+	sig := privKey.Sign(msg)
+
+	assert.True(t, sig.Verify(pubKey, msg))
+	assert.False(t, sig.Verify(pubKey, []byte("wrong key")))
 }
