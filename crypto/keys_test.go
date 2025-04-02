32cbd16b709e4aed8295da535123216d5e0a1863
Artur <art67789@gmail.com>
2025-04-02 23:06:43 +0300

    Test generated private key len correctness

---
+++ crypto/keys_test.go
@@ -0,0 +1,13 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+	privKey := GeneratePrivateKey()
+
+	assert.Equal(t, privateKeyLen, len(privKey.Bytes()))
+}
