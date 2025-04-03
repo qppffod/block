049cd69ac140b9271c62e5c501e9c5b87c8c91b8
Artur <art67789@gmail.com>
2025-04-03 22:47:04 +0300

    Add tests

--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -14,6 +14,19 @@ func TestGeneratePrivateKey(t *testing.T) {
 	assert.Equal(t, publicKeyLen, len(pubKey.Bytes()))
 }
 
+func TestNewPrivateKeyFromString(t *testing.T) {
+	var (
+		seed       = "b492243a9e39c8b36f4102c7b69c9c5bffedcd56f993f0da9a648b8d8925b2e7"
+		privKey    = NewPrivateKeyFromString(seed)
+		addressStr = "4845646149dd7f3a97a8901ca25dcf8cd2dedcc8"
+	)
+
+	assert.Equal(t, privateKeyLen, len(privKey.Bytes()))
+
+	address := privKey.Public().Address()
+	assert.Equal(t, address.String(), addressStr)
+}
+
 func TestPrivateKeySign(t *testing.T) {
 	privKey := GeneratePrivateKey()
 	pubKey := privKey.Public()
@@ -22,5 +35,18 @@ func TestPrivateKeySign(t *testing.T) {
 	sig := privKey.Sign(msg)
 
 	assert.True(t, sig.Verify(pubKey, msg))
+
+	// test with invalid msg
 	assert.False(t, sig.Verify(pubKey, []byte("wrong key")))
+
+	invalidPrivateKey := GeneratePrivateKey()
+	invalidPublicKey := invalidPrivateKey.Public()
+	assert.False(t, sig.Verify(invalidPublicKey, msg))
+}
+
+func TestPublicKeyAddress(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	pubKey := privKey.Public()
+	address := pubKey.Address()
+	assert.Equal(t, addressLen, len(address.Bytes()))
 }
