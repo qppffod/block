e475f4196475e21bcf2c91ea1c3741e7abcd661b
Artur <art67789@gmail.com>
2025-04-04 14:40:09 +0300

    Add funcs to generate certain type structs from bytes

--- crypto/keys.go
+++ crypto/keys.go
@@ -9,6 +9,7 @@ import (
 
 const (
 	privateKeyLen = 64
+	signatureLen  = 64
 	publicKeyLen  = 32
 	seedlen       = 32
 	addressLen    = 20
@@ -72,6 +73,15 @@ type PublicKey struct {
 	key ed25519.PublicKey
 }
 
+func PublicKeyFromBytes(b []byte) *PublicKey {
+	if len(b) != publicKeyLen {
+		panic("invalid public key length")
+	}
+	return &PublicKey{
+		key: ed25519.PublicKey(b),
+	}
+}
+
 func (p *PublicKey) Address() Address {
 	return Address{
 		value: p.key[len(p.key)-addressLen:],
@@ -86,6 +96,15 @@ type Signature struct {
 	value []byte
 }
 
+func SignatureFromBytes(b []byte) *Signature {
+	if len(b) != signatureLen {
+		panic("length of the bytes not equal to 64")
+	}
+	return &Signature{
+		value: b,
+	}
+}
+
 func (s *Signature) Bytes() []byte {
 	return s.value
 }
@@ -98,7 +117,7 @@ type Address struct {
 	value []byte
 }
 
-func (a *Address) Bytes() []byte {
+func (a Address) Bytes() []byte {
 	return a.value
 }
 
