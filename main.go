45f283f837c67f6485acc9544292e7e383482514
Artur <art67789@gmail.com>
2025-04-10 22:40:11 +0300

    Add basic grpc server

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net"
 
+	"github.com/qppfod/block/node"
+	"github.com/qppfod/block/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	node := node.NewNode()
+
+	proto.RegisterNodeServer(grpcServer, node)
+	fmt.Println("node running on port :3000")
+	grpcServer.Serve(ln)
 }
